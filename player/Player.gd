5fa1e255856eb1eac17da0623a6ab31059f5f617
Damamapi <ddamamapi@gmail.com>
2020-06-13 12:14:44 -0500

    C

--- player/Player.gd
+++ player/Player.gd
@@ -1,18 +1,61 @@
 extends KinematicBody2D
 
-var H: = 144
-var T: = 0.15
-var g: = (2*H) / (T * T)
-var xspeed: = 160
+onready var sprite: = $Sprite
 
-var vel : = Vector2.ZERO
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+const FLOOR_NORMAL: = Vector2.UP
+export var speed: = Vector2(400.0, 900.0)
+export var gravity: = 2000.0  
+var _velocity : = Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
-	vel.y += g
-	vel.x = (Input.get_action_strength("left") - Input.get_action_strength("right")) * xspeed
-	move_and_slide(vel * delta, Vector2.UP)
+	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0
+	var direction: = get_direction()
+	_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
+	manage_animations()
+	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
+
+func get_direction() -> Vector2:
+	return Vector2(
+		Input.get_action_strength("right") - Input.get_action_strength("left"),
+		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 1.0
+	)
+
+func manage_animations():
+#	ADMINISTRAR LAS ANIMACIONES PARA SANCHEZITO
+#	puto el que lo lea
+
+	if is_on_floor():
+		if _velocity.x != 0:
+			sprite.play("walking")
+		else:
+			sprite.play("default")
+	else:
+		if _velocity.y > 0:
+			sprite.play("jump")
+		elif _velocity.y < 0:
+			sprite.play("fall")
+	
+	
+	if _velocity.x < 0:
+		sprite.flip_h = 1
+	elif _velocity.x > 0:
+		sprite.flip_h = 0
+	
+func play_animation(anim: String):
+	if sprite.playing != anim:
+		sprite.play(anim)
+
+func calculate_move_velocity(
+		linear_velocity: Vector2,
+		direction: Vector2,
+		speed: Vector2,
+		is_jump_interrupted: bool
+	) -> Vector2:
+	var out: = linear_velocity
+	out.x = speed.x * direction.x
+	out.y += gravity * get_physics_process_delta_time()
+	if direction.y == -1.0:
+		out.y = speed.y * direction.y
+	if is_jump_interrupted:
+		out.y = 0.0
+	return out
